5df7918a349a6a361b0c66695c9364ba85401179
Aashirwad Chauhan <aashirwad.chauhan25@gmail.com>
2024-03-11 19:54:11 +0530

    MongoDB Atlas added

--- db/database.js
+++ db/database.js
@@ -5,6 +5,6 @@ mongoose
   .connect(process.env.MONGO_URI, {
     dbName: "TODOBALLER",
   })
-  .then(() => console.log("Database Connected"))
+  .then((c) => console.log(`Database Connected with ${c.connection.host}`))
   .catch((e) => console.log(e));
 };  
